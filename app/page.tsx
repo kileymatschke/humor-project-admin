99a82f96d44264755952976a15342120c94453f4
kileymatschke <matschke.kiley@gmail.com>
2026-03-05 04:10:47 -0500

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,28 @@
-import { supabase } from "../lib/supabase";
+import { createClient } from "../../lib/supabase/server";
+import Link from "next/link";
+import SignOutButton from "../components/SignOutButton";
+import { adelia } from "../fonts/fonts";
+import { kindergarten } from "../fonts/fonts";
 
 
-export default async function Home() {
-    const { data, error, count } = await supabase
-        .from("profiles")
-        .select("*", { count: "exact" })
-        .limit(10);
+export default async function HomePage() {
+    const supabase = await createClient();
 
-    if (error) {
-        return <pre>{JSON.stringify(error, null, 2)}</pre>
-    }
+    // Who is logged in (this ALWAYS has your email if signed in)
+    const { data: authData } = await supabase.auth.getUser();
+    const myAuthEmail = authData.user?.email ?? "(no auth email)";
+
 
     return (
-        <main>
-            <h1>Emails from profiles</h1>
+        <main style={{ padding: 24 }}>
+            <h1 className={adelia.className}>Home</h1>
+            <div style={{marginBottom:30}}><SignOutButton /></div>
+            {/*<p>Signed in as (from auth): {myAuthEmail}</p>*/}
 
-            <ul>
-                {data?.map((row, i) => (
-                    <li key={i}>{row.email}</li>
-                ))}
-            </ul>
+            <div className={kindergarten.className} style={{ fontSize: "20px", fontWeight: "bold" }}><Link href="../users">View all registered users</Link></div>
+            <div className={kindergarten.className} style={{ fontSize: "20px", fontWeight: "bold" }}><Link href="../images">View all images</Link></div>
+            <div className={kindergarten.className} style={{ fontSize: "20px", fontWeight: "bold" }}><Link href="../captions">View all captions</Link></div>
+
         </main>
     );
 }
